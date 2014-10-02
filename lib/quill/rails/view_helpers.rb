8061b3ef9c3665ccf119a06615dbc76678d802ea
Juan José Conti <jjconti@gmail.com>
2014-10-02 11:37:40 -0300

    Add support for template overriding

--- lib/quill/rails/view_helpers.rb
+++ lib/quill/rails/view_helpers.rb
@@ -13,7 +13,13 @@ module Quill
         @input_name = (name || mod_options[:name])
         @input_id   = quill_sanitize_id(name || mod_options[:id])
         @value      = mod_options[:value].present? ? mod_options[:value] : ""
-        ERB.new(File.read(File.join(source_root, 'template.html.erb'))).result(binding).html_safe
+        custom_template_path = ::Rails.root.join('app', 'views', 'quill', 'template.html.erb')
+        if File.exists? custom_template_path
+          template_path = custom_template_path
+        else
+          template_path = File.join(source_root, 'template.html.erb')
+        end
+        ERB.new(File.read(template_path)).result(binding).html_safe
       end
 
       private
